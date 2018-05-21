db1efafcedafde8bfc2959a96f125506677e952f
Sean Smith <sean.smith@analog.com>
2018-05-21 12:45:47 +0100

    ADDED CSV READ AND EXTRACTION OF RES(TEMP)

--- scratchpadConsole/Program.cs
+++ scratchpadConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using FileHelpers;
 using System;
+using System.Linq;
 
 namespace scratchpadConsole
 {
@@ -9,26 +10,23 @@ namespace scratchpadConsole
         public double EnteredTemperature { get; set; }
         public double MaxTemp { get; set; }
         public double MinTemp { get; set; }
-        public double ResAtT2 { get; set; }
-        public double ResAtT1 { get; set; }
+        public double ResAtMaxTemp { get; set; }
+        public double ResAtMinTemp { get; set; }
     }
 
     public class Program
     {
 
-        public decimal ThermistorInterpelator(InterpolatorInputs ips)
-        {
-            return Convert.ToDecimal(
-                (ips.ResAtT2 - ips.ResAtT1) / (ips.MaxTemp - ips.MinTemp) *
-                (ips.EnteredTemperature - ips.MinTemp) + ips.ResAtT1);
-        }
+        public decimal ThermistorInterpelator(InterpolatorInputs ips) => Convert.ToDecimal(
+                (ips.ResAtMaxTemp - ips.ResAtMinTemp) / (ips.MaxTemp - ips.MinTemp) *
+                (ips.EnteredTemperature - ips.MinTemp) + ips.ResAtMinTemp);
 
         static void Main()
         {
             Program program = new Program();
 
             var engine = new FileHelperEngine<ResistanceData>();
-            var result = engine.ReadFile("44006_44031.csv");
+            var omega = engine.ReadFile("44006_44031.csv");
             var panasonic = engine.ReadFile("Panasonic_Thermistor.csv");
 
             foreach (ResistanceData data in panasonic)
@@ -41,17 +39,36 @@ namespace scratchpadConsole
                 EnteredTemperature = -70.2,
                 MaxTemp = -70,
                 MinTemp = -71,
-                ResAtT2 = 1694000,
-                ResAtT1 = 1821000
+                ResAtMaxTemp = 1694000,
+                ResAtMinTemp = 1821000
             };
+
+            InterpolatorInputs ip2 = new InterpolatorInputs();
+            double EnteredTemperature = -75.5;
+            ip2.EnteredTemperature = EnteredTemperature;
+            ip2.MaxTemp = Math.Ceiling(EnteredTemperature);
+            ip2.MinTemp = Math.Floor(EnteredTemperature);
+            ip2.ResAtMaxTemp = omega.FirstOrDefault(x => x.Temerature == ip2.MaxTemp).Resistance;
+            ip2.ResAtMinTemp = omega.FirstOrDefault(x => x.Temerature == ip2.MinTemp).Resistance;
+
 
             Console.WriteLine(
                 $"Entered Temperature: {ip.EnteredTemperature}" + "\n" +
                 $"Max Temperature: {ip.MaxTemp}" + "\n" +
                 $"Min Temperature: {ip.MinTemp}" + "\n" +
-                $"Resistance at Max Temperature: {ip.ResAtT2}" + "\n" +
-                $"Resistance at Min Temperature: {ip.ResAtT1}" + "\n" +
+                $"Resistance at Max Temperature: {ip.ResAtMaxTemp}" + "\n" +
+                $"Resistance at Min Temperature: {ip.ResAtMinTemp}" + "\n" +
                 $"Interpolated resistance: {program.ThermistorInterpelator(ip)}"
+                );
+            Console.WriteLine();
+
+            Console.WriteLine(
+                $"Entered Temperature: {ip2.EnteredTemperature}" + "\n" +
+                $"Max Temperature: {ip2.MaxTemp}" + "\n" +
+                $"Min Temperature: {ip2.MinTemp}" + "\n" +
+                $"Resistance at Max Temperature: {ip2.ResAtMaxTemp}" + "\n" +
+                $"Resistance at Min Temperature: {ip2.ResAtMinTemp}" + "\n" +
+                $"Interpolated resistance: {program.ThermistorInterpelator(ip2)}"
                 );
         }
     }
