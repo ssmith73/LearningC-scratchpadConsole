c72fcb3424dd811bb8ed608c878814b3cb07bfde
Sean Smith <ssmith73@gmail.com>
2018-05-24 19:04:04 +0100

    FIXED BUG IN DLL (../REPOS/INTERPOLATOR)

--- scratchpadConsole/Program.cs
+++ scratchpadConsole/Program.cs
@@ -1,30 +1,58 @@
 ﻿using FileHelpers;
 using System;
 using System.Linq;
+using Interpolation;
 
 namespace scratchpadConsole
 {
 
-    public struct InterpolatorInputs
-    {
-        public double EnteredTemperature { get; set; }
-        public double MaxTemp { get; set; }
-        public double MinTemp { get; set; }
-        public double ResAtMaxTemp { get; set; }
-        public double ResAtMinTemp { get; set; }
-    }
 
     public class Program
     {
 
-        public decimal ThermistorInterpelator(InterpolatorInputs ips) => Convert.ToDecimal(
-                (ips.ResAtMaxTemp - ips.ResAtMinTemp) / (ips.MaxTemp - ips.MinTemp) *
-                (ips.EnteredTemperature - ips.MinTemp) + ips.ResAtMinTemp);
 
         static void Main()
         {
             Program program = new Program();
 
+            /*! .  
+
+                B equation: T = B / ln(R/R#)
+
+                  Where R# = R0e(-B/T0)
+                  Where R0 = Resistance at T0
+                  Where T0 = Temperature at 25'C (298.15K)
+
+                Steinhart-Hart equation: 1/T = a + b.ln(R) + c(ln(R))3
+
+                  T = absolute-temperature, R is the associated thermistor resistance
+                  Where A,B,C are Steinhart-Hart parameters, specific for
+                  a given thermistor
+
+             */
+
+            // Initialize the interpolation manager using the sample data
+            InterpolationManager im = 
+                new InterpolationManager("Panasonic_Thermistor.csv");
+
+            // Interpolate using the gixen X axis, using the given Y axis, 
+            // and using the given X value (returns the associated Y value)
+            double val = im.Interpolate("Temperature (degC)", "Resistance (ohms)", 22);
+            
+
+            // Interpolate ASSSUMING the first column is the X axis,
+            // using the given Y axis, and using the given X value (returns the associated Y value)
+            double val2 = im.Interpolate("Resistance (ohms)", 50);
+
+            // Interpolate ASSSUMING the first column is the X axis, 
+            // ASSUMING the second column is the Y axis, and using the 
+            // given X value (returns the associated Y value)
+            double val3 = im.Interpolate(111);
+
+
+
+            #region LINEAR INTERPOLATION - FROM EXCEL EQUATION
+            // Extract linear interpolation
             var engine = new FileHelperEngine<ResistanceData>();
             var omega = engine.ReadFile("44006_44031.csv");
             var panasonic = engine.ReadFile("Panasonic_Thermistor.csv");
@@ -44,7 +72,7 @@ namespace scratchpadConsole
             };
 
             InterpolatorInputs ip2 = new InterpolatorInputs();
-            double EnteredTemperature = -75.5;
+            double EnteredTemperature = -70.44;
             ip2.EnteredTemperature = EnteredTemperature;
             ip2.MaxTemp = Math.Ceiling(EnteredTemperature);
             ip2.MinTemp = Math.Floor(EnteredTemperature);
@@ -70,6 +98,19 @@ namespace scratchpadConsole
                 $"Resistance at Min Temperature: {ip2.ResAtMinTemp}" + "\n" +
                 $"Interpolated resistance: {program.ThermistorInterpelator(ip2)}"
                 );
+
+            #endregion
         }
+        public decimal ThermistorInterpelator(InterpolatorInputs ips) => Convert.ToDecimal(
+                (ips.ResAtMaxTemp - ips.ResAtMinTemp) / (ips.MaxTemp - ips.MinTemp) *
+                (ips.EnteredTemperature - ips.MinTemp) + ips.ResAtMinTemp);
+    }
+    public struct InterpolatorInputs
+    {
+        public double EnteredTemperature { get; set; }
+        public double MaxTemp { get; set; }
+        public double MinTemp { get; set; }
+        public double ResAtMaxTemp { get; set; }
+        public double ResAtMinTemp { get; set; }
     }
 }
